7bd92bfd5b890499c3feffe0693453ced867a4c3
Terence <tjsaverin98@live.com>
2025-05-09 10:26:28 -0500

    requesting data from backend error

---
+++ index.js
@@ -0,0 +1,30 @@
+// API 1: "https://jsonplaceholder.typicode.com/users"
+// API 2: "https://jsonplaceholder.typicode.com/posts?userId=:id"
+
+async function main() {
+  const users = await fetch("https://jsonplaceholder.typicode.com/users");
+  const usersData = await users.json();
+  const userListEl = document.querySelector(".user-list")
+  console.log(usersData)
+  userListEl.innerHTML = usersData.map((user) => userHTML(user)).join("");
+}
+
+main();
+
+function showUserPost(id) {
+    window.location.href = 
+    console.log(id);
+}
+
+function userHTML(user) {
+    return `<div class="user-card" onclick="showUserPosts(${user.id})">
+    <div class="user-card__container">
+      <h3>${user.name}</h4>
+        <p><b>Email:</b> ${user.email}</p>
+        <p><b>Phone:</b> ${user.phone} </p>
+        <p><b>Website:</b> <a href="https://${user.website}" target="_blank">
+        ${user.website}
+        </a></p>
+    </div>
+   </div>`;
+}
